1360096c4412c0363786b15488199d9d6816bce3
Redither <slavonchik68@gmail.com>
2023-04-12 15:24:34 +0300

    added template of answer for new request

--- bot.py
+++ bot.py
@@ -2,6 +2,10 @@ import time
 import logging
 import asyncio
 import os
+from datetime import datetime
+import calendar
+import requests
+import json
 
 from dotenv import load_dotenv
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
@@ -11,27 +15,53 @@ if os.path.exists(dotenv_path):
 from aiogram import Bot, Dispatcher, executor, types
 
 TOKEN = os.environ.get('TOKEN')
-MSG = "Программировал ли ты сегодня, {}?"
 USER = os.environ.get('USER_ID')
 CHAT = os.environ.get('CHAT_ID')
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot=bot)
 
 @dp.message_handler(commands=['start'])
-
 async def start_handler(message: types.Message):
+    # должно логать действия
     user_id = message.from_user.id
     user_name = message.from_user.first_name
     user_full_name = message.from_user.full_name
     logging.info(f'{user_id} {user_full_name} {time.asctime()}')
-    if user_id != USER:
-        await message.reply(f"Пошел нахуй, {user_full_name}!")
-    else:
-        await message.reply(f"Добро пожаловать, {user_full_name}!")
 
-    for i in range(7):
-        await asyncio.sleep(60*60*24)
-        await bot.send_message(user_id, MSG.format(user_name))
+    if str(message.from_user.id) not in USER:
+        await message.reply(f"Пошел нахуй, {user_full_name}!")
+        return
+
+    else:
+        await message.reply(f"Добро пожаловть, {user_full_name}!")
+
+@dp.message_handler(commands=['svodka'])
+async def svodka_handler(message: types.Message):
+    today = datetime.now().date()
+    first_day = today.replace(day=1)
+    last_day = today.replace(day = calendar.monthrange(2023, 4)[1])
+    REQUEST = {
+        "date_from": first_day.strftime('%Y-%M-%D'),
+        "date_to": last_day.strftime('%Y-%M-%D'),
+        "metrics": [
+            "revenue"
+        ],
+        "dimension": [
+            "day",
+            "month"
+        ],
+        "filters": [ ],
+        "sort": [
+            {
+                "key": "revenue",
+                "order": "DESC"
+            }
+        ],
+        "limit": 1000,
+        "offset": 0
+    }
+    await message.reply(f'{today} + {type(today)}, {first_day} + {type(first_day)}, {last_day} + {type(last_day)}')
+
 
 if __name__ == "__main__":
     executor.start_polling(dp)
