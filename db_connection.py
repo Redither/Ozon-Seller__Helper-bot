99465d0ef0f5af8056b15d50eed7a0649ac36aed
Redither <slavonchik68@gmail.com>
2023-04-14 00:30:36 +0300

    added add plan query

--- db_connection.py
+++ db_connection.py
@@ -1,23 +1,26 @@
 import sqlite3
 
-try:
-    sqlite_connection = sqlite3.connect('db.sqlite')
-    sqlite_create_table_query = '''CREATE TABLE sales_plan (
-                                    id TEXT PRIMARY KEY,
-                                    plan INTEGER NOT NULL
-                                );'''
-    
-    cursor = sqlite_connection.cursor()
-    print("База данных создана и успешно подключена к SQLite")
-    cursor.execute(sqlite_create_table_query)
-    sqlite_connection.commit()
-    print("Таблица создана")
-    
-    cursor.close()
+def insert_sales_plan(id, plan):
+    try:
+        sqlite_connection = sqlite3.connect('db.sqlite')
+        cursor = sqlite_connection.cursor()
+        print("Подключено к SQLite")
 
-except sqlite3.Error as error:
-    print("Ошибка при подключении к sqlite", error)
-finally:
-    if (sqlite_connection):
-        sqlite_connection.close()
-        print("Соединение с SQLite закрыто")
+        sqlite_insert_with_param = '''INSERT INTO sales_plan(id, plan)
+                                    VALUES (?, ?);'''
+    
+        data_tuple = (id, plan)
+        cursor.execute(sqlite_insert_with_param, data_tuple)
+        sqlite_connection.commit()
+        print("Переменные внесены")
+    
+        cursor.close()
+
+    except sqlite3.Error as error:
+        print("Ошибка при подключении к sqlite", error)
+    finally:
+        if (sqlite_connection):
+            sqlite_connection.close()
+            print("Соединение с SQLite закрыто")
+
+# insert_sales_plan("2023-03", 2000000)
