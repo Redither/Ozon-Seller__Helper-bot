045354fe5a92efc14396bb614169508ffae0cb6e
Redither <slavonchik68@gmail.com>
2023-04-14 00:51:51 +0300

    added update plan query

--- db_connection.py
+++ db_connection.py
@@ -23,4 +23,29 @@ def insert_sales_plan(id, plan):
             sqlite_connection.close()
             print("Соединение с SQLite закрыто")
 
+def update_sales_plan(id, plan):
+    try:
+        sqlite_connection = sqlite3.connect('db.sqlite')
+        cursor = sqlite_connection.cursor()
+        print("Подключен к SQLite")
+
+        sql_update_query = """UPDATE sales_plan 
+                                SET 
+                                    plan = ? 
+                                WHERE 
+                                id = ?;"""
+        data = (plan, id)
+        cursor.execute(sql_update_query, data)
+        sqlite_connection.commit()
+        print("Запись успешно обновлена")
+        cursor.close()
+
+    except sqlite3.Error as error:
+        print("Ошибка при работе с SQLite", error)
+    finally:
+        if sqlite_connection:
+            sqlite_connection.close()
+            print("Соединение с SQLite закрыто")
+
 # insert_sales_plan("2023-03", 2000000)
+# update_sales_plan("2023-03", 2500000)
