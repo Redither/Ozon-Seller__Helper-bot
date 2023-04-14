85696018029f1617ba6877673c808a3ac55515a2
Redither <slavonchik68@gmail.com>
2023-04-14 08:00:36 +0300

    Update db_connection.py

--- db_connection.py
+++ db_connection.py
@@ -13,8 +13,27 @@ def add_sales_plan(id, plan):
         cursor.execute(sqlite_insert_with_param, data_tuple)
         sqlite_connection.commit()
         print("Переменные внесены")
-    
         cursor.close()
+
+    except sqlite3.Error as error:
+        print("Ошибка при подключении к sqlite", error)
+    finally:
+        if (sqlite_connection):
+            sqlite_connection.close()
+            print("Соединение с SQLite закрыто")
+
+def get_sales_plan(id):
+    try:
+        sqlite_connection = sqlite3.connect('db.sqlite')
+        cursor = sqlite_connection.cursor()
+        print("Подключено к SQLite")
+
+        sqlite_get_with_param = '''SELECT plan FROM sales_plan WHERE id = ?;'''
+    
+        answ = cursor.execute(sqlite_get_with_param, [id]).fetchone()[0]
+        print("Данные получены")
+        cursor.close()
+        return answ
 
     except sqlite3.Error as error:
         print("Ошибка при подключении к sqlite", error)
