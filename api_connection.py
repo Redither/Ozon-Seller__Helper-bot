300fadcaefcb7adbd46da542ffe2e78fe3a3bb8f
Redither <slavonchik68@gmail.com>
2023-04-20 18:48:50 +0300

    added requests for prev months stat data

--- api_connection.py
+++ api_connection.py
@@ -14,9 +14,16 @@ KEY = os.environ.get('API_KEY')
 today = str(datetime.now().date())
 first_day = str(datetime.now().date().replace(day=1))
 
-def get_sales():
-    url = "https://api-seller.ozon.ru/v1/analytics/data"
+url = "https://api-seller.ozon.ru/v1/analytics/data"
+    # укажите заголовки запроса
+headers = {
+    "Content-Type": "application/json",
+    # укажите свой магазин ID и API ключ
+    "Client-Id": SELLER,
+    "Api-Key": KEY
+}
 
+def get_sales_month():    
     # укажите требуемые данные для создания товара в виде словаря
     request_data = {
     "date_from": first_day,
@@ -39,12 +46,67 @@ def get_sales():
     "limit": 1000,
     "offset": 0
     }
-    # укажите заголовки запроса
-    headers = {
-    "Content-Type": "application/json",
-    # укажите свой магазин ID и API ключ
-    "Client-Id": SELLER,
-    "Api-Key": KEY
+
+    data = json.dumps(request_data).encode('utf-8')
+    req = request.Request(url, headers=headers, data=data, method="POST")
+    r = request.urlopen(req)
+    # получите ответ в формате json
+    result = json.loads(r.read().decode('utf-8'))['result']
+    return result
+
+
+def get_sales_today():
+    request_data = {
+        "date_from": today,
+        "date_to": today,
+        "metrics": [
+            "revenue",
+            "ordered_units"
+        ],
+        "dimension": [
+            "sku",
+            "day"
+        ],
+        "filters": [],
+        "sort": [
+            {
+                "key": "revenue",
+                "order": "DESC"
+            }
+        ],
+        "limit": 1000,
+        "offset": 0
+    }
+    data = json.dumps(request_data).encode('utf-8')
+    req = request.Request(url, headers=headers, data=data, method="POST")
+    r = request.urlopen(req)
+    # получите ответ в формате json
+    result = json.loads(r.read().decode('utf-8'))['result']
+    return result
+
+
+def get_sales_today__last():
+    # укажите требуемые данные для создания товара в виде словаря
+    request_data = {
+    "date_from": first_day,
+    "date_to": today,
+    "metrics": [
+        "revenue",
+        "ordered_units"
+    ],
+    "dimension": [
+        "sku",
+        "month"
+    ],
+    "filters": [],
+    "sort": [
+        {
+            "key": "revenue",
+            "order": "DESC"
+        }
+    ],
+    "limit": 1000,
+    "offset": 0
     }
 
     data = json.dumps(request_data).encode('utf-8')
@@ -52,5 +114,36 @@ def get_sales():
     r = request.urlopen(req)
     # получите ответ в формате json
     result = json.loads(r.read().decode('utf-8'))['result']
+    return result
 
+
+def get_sales_month__last():    
+    # укажите требуемые данные для создания товара в виде словаря
+    request_data = {
+    "date_from": first_day,
+    "date_to": today,
+    "metrics": [
+        "revenue",
+        "ordered_units"
+    ],
+    "dimension": [
+        "sku",
+        "month"
+    ],
+    "filters": [],
+    "sort": [
+        {
+            "key": "revenue",
+            "order": "DESC"
+        }
+    ],
+    "limit": 1000,
+    "offset": 0
+    }
+
+    data = json.dumps(request_data).encode('utf-8')
+    req = request.Request(url, headers=headers, data=data, method="POST")
+    r = request.urlopen(req)
+    # получите ответ в формате json
+    result = json.loads(r.read().decode('utf-8'))['result']
     return result
